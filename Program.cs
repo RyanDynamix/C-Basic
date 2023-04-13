ebf0c2ec56efc3bd484b82b279fbffd3b7b78db2
RyanDynamix <ryannguyen070823@gmail.com>
2023-04-13 20:56:05 +0700

    SystemTime

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SystemTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("DateTime Now is: " + localDate);
+        }
+    }
+}
